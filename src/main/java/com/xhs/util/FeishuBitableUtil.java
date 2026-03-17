391df8b9c7d5a7e235cde9310cb9bc5ea40a195b
Le-Zhang <15674790082@163.com>
2026-03-17 15:56:39 +0800

    fix[type]：修改时间类型

--- src/main/java/com/xhs/util/FeishuBitableUtil.java
+++ src/main/java/com/xhs/util/FeishuBitableUtil.java
@@ -67,7 +67,7 @@ public class FeishuBitableUtil {
      * 用于批量匹配：传入日期条件，返回该日期下所有记录的 {计划ID: record_id}
      */
     public static Map<String, String> searchRecords(String appToken, String tableId,
-                                                     String filterFieldName, String filterFieldValue,
+                                                     String filterFieldName, Object filterFieldValue,
                                                      String keyFieldName,
                                                      String tenantAccessToken) {
         Map<String, String> result = new HashMap<>();
